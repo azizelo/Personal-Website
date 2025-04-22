495d3908c966c84441d1d45445559e4c7d4d3070
Aziz El Ouaqid <33883774+azizelo@users.noreply.github.com>
2025-04-22 00:19:26 -0700

    cleaning css issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   base: '/Personal-Website/',
   integrations: [tailwind()],
-  // Optional: if you're not using SSR
   output: 'static',
 });
