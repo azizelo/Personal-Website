a4b562d26e08cbf87dcb5ec5d71564e9aaff5b9a
Aziz El Ouaqid <33883774+azizelo@users.noreply.github.com>
2025-05-03 13:16:02 -0700

    meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://azizelo.github.io/Personal-Website',
   base: '/Personal-Website/',
   integrations: [tailwind()],
   output: 'static',
